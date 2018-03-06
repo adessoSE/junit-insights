c5f28b3943a3eada188e7f1988e7db48aedd476c
SilasMahler <simah002@stud.fh-dortmund.de>
2018-03-06 09:59:17 +0100

    add base-project

---
+++ src/main/java/de/adesso/junitinsights/JunitInsightsApplication.java
@@ -0,0 +1,12 @@
+package de.adesso.junitinsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitInsightsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitInsightsApplication.class, args);
+	}
+}
