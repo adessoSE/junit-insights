10d250b767bd568ca589ea7a255c442844af6ee0
SilasMahler <simah002@stud.fh-dortmund.de>
2018-03-06 17:51:56 +0100

    updated jpa and ref<actoring

--- src/main/kotlin/de/adesso/junitinsights/TimestampEvent.kt
+++ src/main/kotlin/de/adesso/junitinsights/TimestampEvent.kt
@@ -1,10 +1,10 @@
 package de.adesso.junitinsights
 
 import java.util.*
-import javax.persistence.EnumType
-import javax.persistence.Enumerated
+import javax.persistence.*
 
-data class TimestampEvent (var id: Long, var timestamp: Date, var event: String, var testClass: String, var testMethod: String) {
+@Entity
+data class TimestampEvent (@Id @GeneratedValue var id: Long, var timestamp: Date, var event: String, var testClass: String, var testMethod: String) {
 
     override fun toString(): String {
         return "TimestampEvent(id=$id, timestamp=$timestamp, event=$event, testClass='$testClass', testMethod='$testMethod')"
