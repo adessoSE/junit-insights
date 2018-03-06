9e2ff8f5ed1fcb574ff2124b6df9c07ce426b1ad
florianluediger <florian.luediger@googlemail.com>
2018-03-06 13:35:36 +0100

    Migrate from JUnit version 4 to version 5

--- src/test/kotlin/de/adesso/junitinsights/HelloControllerTest.kt
+++ src/test/kotlin/de/adesso/junitinsights/HelloControllerTest.kt
@@ -1,20 +1,20 @@
 package de.adesso.junitinsights
 
 import org.hamcrest.Matchers.equalTo
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-import org.junit.Test
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 
-@RunWith(value = SpringRunner::class)
+@ExtendWith(SpringExtension::class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class HelloControllerTest {
