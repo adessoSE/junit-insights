9ae6591767d874f73a180f96eaef39b496df005b
SilasMahler <simah002@stud.fh-dortmund.de>
2018-03-12 20:10:12 +0100

    refactore extension and logger

--- src/main/kotlin/de/adesso/junitinsights/extensions/SpringInsightExtension.kt
+++ src/main/kotlin/de/adesso/junitinsights/extensions/SpringInsightExtension.kt
@@ -5,56 +5,45 @@ import de.adesso.junitinsights.TimestampEventService
 import org.junit.jupiter.api.extension.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.test.context.junit.jupiter.SpringExtension
-import javax.annotation.Resource
+import org.springframework.stereotype.Component
 
-class SpringInsightExtension: SpringExtension() {
+@Component
+class SpringInsightExtension(
+):      BeforeAllCallback, AfterAllCallback,
+        BeforeEachCallback, AfterEachCallback,
+        BeforeTestExecutionCallback, AfterTestExecutionCallback
+{
+
     companion object {
         var logger : Logger = LoggerFactory.getLogger(this::class.java)
     }
 
-    @Resource
-    lateinit var timestampEventService: TimestampEventService
-
     override fun beforeAll(context: ExtensionContext) {
-        super.beforeAll(context)
-        logger.info("### beforeAll: class - " + context.testClass + " ###"
-                + " method - " + context.testMethod + " ###")
-        //timestampEventService.createEventNow(eventType = EventType.TEST_CLASS_START, context = context)
-        //TODO Adjust
+        //TODO Instanciate timestampEventService here, before container starts or tests are executed.
+        //TODO Also put the object in the extensioncontext, so its accessible in the testcontainer
+        logger.info("### beforeAll: class - ${context.testClass} ### method - ${context.testMethod} ###")
+
+        //  timestampEventService.createEventNow(eventType = EventType.TEST_CLASS_START, context = context)
     }
 
     override fun afterAll(context: ExtensionContext) {
-        super.afterAll(context)
-        logger.info("### afterAll: class - " + context.testClass + " ###"
-                + " method - " + context.testMethod + " ###")
-        //timestampEventService.createEventNow(eventType = EventType.TEST_CLASS_END, context = context)
-        //TODO Adjust
+        logger.info("### afterAll: class - ${context.testClass} ### method - ${context.testMethod} ###")
     }
 
     override fun beforeEach(context: ExtensionContext) {
-        super.beforeEach(context)
-        logger.info("### beforeEach: class - " + context.testClass + " ###"
-                + " method - " + context.testMethod + " ###")
-        timestampEventService.createEventNow(eventType = EventType.TEST_METHOD_START, context = context)
+        logger.info("### beforeEach: class - ${context.testClass} ### method - ${context.testMethod} ###")
     }
 
     override fun afterEach(context: ExtensionContext) {
-        super.afterEach(context)
-        logger.info("### afterEach: class - " + context.testClass + " ###"
-                + " method - " + context.testMethod + " ###")
+        logger.info("### afterEach: class - ${context.testClass} ### method - ${context.testMethod} ###")
     }
 
     override fun beforeTestExecution(context: ExtensionContext) {
-        super.beforeTestExecution(context)
-        logger.info("### beforeTestExecution: class - " + context.testClass + " ###"
-                + " method - " + context.testMethod + " ###")
+        logger.info("### beforeTestExecution: class - ${context.testClass} ### method - ${context.testMethod} ###")
     }
 
     override fun afterTestExecution(context: ExtensionContext) {
-        super.afterTestExecution(context)
-        logger.info("### afterTestExecution: class - " + context.testClass + " ###"
-                + " method - " + context.testMethod + " ###")
+        logger.info("### afterTestExecution: class - ${context.testClass} ### method - ${context.testMethod} ###")
     }
 
 }
