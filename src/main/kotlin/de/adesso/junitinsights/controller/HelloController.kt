c8edecdbef2dfabad5e97d80ce57bd1037b98020
SilasMahler <simah002@stud.fh-dortmund.de>
2018-03-12 21:51:42 +0100

    replaced controller

--- src/main/kotlin/de/adesso/junitinsights/controller/HelloController.kt
+++ src/main/kotlin/de/adesso/junitinsights/controller/HelloController.kt
@@ -1,4 +1,4 @@
-package de.adesso.junitinsights
+package de.adesso.junitinsights.controller
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
