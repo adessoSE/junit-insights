759eeb6ec4c47fc918836b1374efc8161218382d
SilasMahler <simah002@stud.fh-dortmund.de>
2018-03-12 19:57:37 +0100

    remove imports

--- src/main/kotlin/de/adesso/junitinsights/JunitInsightsApplication.kt
+++ src/main/kotlin/de/adesso/junitinsights/JunitInsightsApplication.kt
@@ -2,7 +2,6 @@ package de.adesso.junitinsights
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import javax.annotation.Resource
 
 @SpringBootApplication
 class JunitInsightsApplication
