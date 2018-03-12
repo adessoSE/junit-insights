2f8736a528c8c72e5d6fb7722dfea7751a9db594
SilasMahler <simah002@stud.fh-dortmund.de>
2018-03-12 19:58:21 +0100

    asjust to jpa repository

--- src/main/kotlin/de/adesso/junitinsights/TimestampEventRepository.kt
+++ src/main/kotlin/de/adesso/junitinsights/TimestampEventRepository.kt
@@ -1,7 +1,8 @@
 package de.adesso.junitinsights
 
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface TimestampEventRepository : CrudRepository<TimestampEvent, Long>
+open interface TimestampEventRepository : JpaRepository<TimestampEvent, Long>
