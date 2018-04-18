7aba5570df6acbd42711eb35f08e432a067a5934
Florian Lüdiger <florian.luediger@googlemail.com>
2018-04-18 14:29:31 +0200

    Generate module structure and add tester to the main project

---
+++ tester/src/test/kotlin/de/adesso/junitinsightstester/JUnitInsightsTesterTests.kt
@@ -0,0 +1,21 @@
+package de.adesso.junitinsightstester
+
+import de.adesso.junitinsights.annotations.JUnitInsights
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+
+/**
+ * Some basic test
+ */
+@SpringBootTest
+@JUnitInsights
+class JUnitInsightsTesterTests {
+
+	@Test
+	fun contextLoads() {
+		assertEquals(1,1);
+	}
+
+}
