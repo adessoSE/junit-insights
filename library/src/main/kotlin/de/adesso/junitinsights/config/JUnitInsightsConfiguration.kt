0ebe39f9c1d4de576c70e7280743d70eb62a65c6
SilasMahler <simah002@stud.fh-dortmund.de>
2018-05-03 15:49:03 +0200

    remove import

--- library/src/main/kotlin/de/adesso/junitinsights/config/JUnitInsightsConfiguration.kt
+++ library/src/main/kotlin/de/adesso/junitinsights/config/JUnitInsightsConfiguration.kt
@@ -4,7 +4,6 @@ import de.adesso.junitinsights.listener.SpringContextListener
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.PropertySource
 
 @Configuration
 @EnableConfigurationProperties(JUnitInsightsReportProperties::class)
