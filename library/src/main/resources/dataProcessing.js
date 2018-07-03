a9d16b658204fc8af54dbad581e1f7ce40692a98
Thomas Buß <Thomas.Buss@adesso.de>
2018-07-03 13:07:23 +0200

    Refactor dataProcessing.js

--- library/src/main/resources/dataProcessing.js
+++ library/src/main/resources/dataProcessing.js
@@ -64,25 +64,29 @@ function calculateDurations(classesTimestamps) {
         testBegin = 0;
 
         currentClassTimestamps.forEach(function (currentEvent, index) {
-            if (currentEvent[1] === "before test execution") {    // "before test execution" is the first event for each test
-                if (testBegin === 0) {
-                    testBegin = currentEvent[0];
-                } else {
-                    console.error("An error occurred while parsing the file!");
-                }
-            } else if (currentEvent[1] === "after test execution") {  // "after test execution" marks termination of the test
-                if (testBegin === 0) {
-                    console.error("An error occurred while parsing the file!");
-                } else {
-                    testDurations.push(currentEvent[0] - testBegin);
-                    testNames.push(currentEvent[3]);
-                    testDurationSum += currentEvent[0] - testBegin;
-                    testBegin = 0;
-                    testSuccess.push(currentEvent[4] === "false")
-                }
-            } else if (currentEvent[1] === "context refreshed") {
-                currentClass.spring += currentEvent[0] - currentClassTimestamps[index-1][0];
-                currentClass.newContexts++;
+            switch (currentEvent[1]) {
+                case "before test execution": {
+                    if (testBegin === 0) {
+                        testBegin = currentEvent[0];
+                    } else {
+                        console.error("An error occurred while parsing the file!");
+                    }
+                }; break;
+                case "after test execution": {
+                    if (testBegin === 0) {
+                        console.error("An error occurred while parsing the file!");
+                    } else {
+                        testDurations.push(currentEvent[0] - testBegin);
+                        testNames.push(currentEvent[3]);
+                        testDurationSum += currentEvent[0] - testBegin;
+                        testBegin = 0;
+                        testSuccess.push(currentEvent[4] === "false")
+                    }
+                }; break;
+                case "context refreshed": {
+                    currentClass.spring += currentEvent[0] - currentClassTimestamps[index-1][0];
+                    currentClass.newContexts++;
+                };
             }
         });
 
