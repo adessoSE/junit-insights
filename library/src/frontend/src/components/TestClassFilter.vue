9157261336a75a70795f99e455f807316fd46caa
Florian Lüdiger <florian.luediger@googlemail.com>
2018-10-30 15:49:31 +0100

    Fix indentation

--- library/src/frontend/src/components/TestClassFilter.vue
+++ library/src/frontend/src/components/TestClassFilter.vue
@@ -1,79 +1,79 @@
 <template>
     <div class="filter">
         <div class="grid-wrapper">
-                <div class="right-addon input-group">
-                    <input type="number" class="form-control" v-model="minSpringShare" @change="updateFunc"
-                        placeholder="Min. Spring share"
-                        data-toggle="tooltip" title="The minimum portion of time spent on Spring initialization">
-                    <div class="input-group-append"><span class="input-group-text">%</span></div>
-                </div>
-                <div class="right-addon input-group">
-                    <input type="number" class="form-control" v-model="maxSpringShare" @change="updateFunc"
-                        placeholder="Max. Spring share"
-                        data-toggle="tooltip" title="The maximum portion of time spent on Spring initialization">
-                    <div class="input-group-append"><span class="input-group-text">%</span></div>
-                </div>
-                <div class="right-addon input-group">
-                    <input type="number" class="form-control" v-model="minTime" @change="updateFunc"
-                           placeholder="Min. test time"
-                           data-toggle="tooltip" title="The minimum total duration of a test class">
-                    <div class="input-group-append"><span class="input-group-text">ms</span></div>
-                </div>
-                <div class="right-addon input-group">
-                    <input type="number" class="form-control" v-model="maxTime" @change="updateFunc"
-                           placeholder="Max. test time"
-                           data-toggle="tooltip" title="The maximum total duration of a test class">
-                    <div class="input-group-append"><span class="input-group-text">ms</span></div>
-                </div>
-                <div class="button-group input-group" data-toggle="tooltip"
-                     title="Filter tests that create their own Spring context and those that don't">
-                    <button type="button" class="form-control dropdown-toggle" data-toggle="dropdown"
-                            style="white-space: nowrap;">
-                        Context creation<span class="caret" ></span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li class="dropdown-checkbox">
-                            <input type="checkbox" id="include_spring_tests" checked
-                                   @change="updateFunc" v-model="includeSpring"/>
-                            <label for="include_spring_tests"
-                                   >Include Spring tests</label>
-                        </li>
-                        <li class="dropdown-checkbox">
-                            <input type="checkbox" id="include_non_spring_tests" checked
-                                   @change="updateFunc" v-model="includeNonSpring"/>
-                            <label for="include_non_spring_tests"
-                                   >Include non Spring tests</label>
-                        </li>
-                    </ul>
-                </div>
-                <div class="button-group input-group" data-toggle="tooltip"
-                     title="Filter tests based on their success">
-                    <button type="button" class="form-control dropdown-toggle" data-toggle="dropdown"
-                            style="white-space: nowrap;">
-                        Test success<span class="caret" ></span>
-                    </button>
-                    <ul class="dropdown-menu">
-                        <li class="dropdown-checkbox">
-                            <input type="checkbox" id="include_succeeded_tests" checked
-                                   @change="updateFunc" v-model="includeSuccess"/>
-                            <label for="include_succeeded_tests"
-                                   >Include succeeded tests</label>
-                        </li>
-                        <li class="dropdown-checkbox">
-                            <input type="checkbox" id="include_partial_tests" checked
-                                   @change="updateFunc" v-model="includePartial"/>
-                            <label for="include_partial_tests"
-                                   >Include partial succeeded tests</label>
-                        </li>
-                        <li class="dropdown-checkbox">
-                            <input type="checkbox" id="include_failed_tests" checked
-                                   @change="updateFunc" v-model="includeFailure"/>
-                            <label for="include_failed_tests"
-                                   >Include failed tests</label>
-                        </li>
-                    </ul>
-                </div>
-                <button type="button" class="btn btn-light input-group" v-on:click="clearClicked()" style="background-color: #FFFFFF;">Clear</button>
+            <div class="right-addon input-group">
+                <input type="number" class="form-control" v-model="minSpringShare" @change="updateFunc"
+                    placeholder="Min. Spring share"
+                    data-toggle="tooltip" title="The minimum portion of time spent on Spring initialization">
+                <div class="input-group-append"><span class="input-group-text">%</span></div>
+            </div>
+            <div class="right-addon input-group">
+                <input type="number" class="form-control" v-model="maxSpringShare" @change="updateFunc"
+                    placeholder="Max. Spring share"
+                    data-toggle="tooltip" title="The maximum portion of time spent on Spring initialization">
+                <div class="input-group-append"><span class="input-group-text">%</span></div>
+            </div>
+            <div class="right-addon input-group">
+                <input type="number" class="form-control" v-model="minTime" @change="updateFunc"
+                       placeholder="Min. test time"
+                       data-toggle="tooltip" title="The minimum total duration of a test class">
+                <div class="input-group-append"><span class="input-group-text">ms</span></div>
+            </div>
+            <div class="right-addon input-group">
+                <input type="number" class="form-control" v-model="maxTime" @change="updateFunc"
+                       placeholder="Max. test time"
+                       data-toggle="tooltip" title="The maximum total duration of a test class">
+                <div class="input-group-append"><span class="input-group-text">ms</span></div>
+            </div>
+            <div class="button-group input-group" data-toggle="tooltip"
+                 title="Filter tests that create their own Spring context and those that don't">
+                <button type="button" class="form-control dropdown-toggle" data-toggle="dropdown"
+                        style="white-space: nowrap;">
+                    Context creation<span class="caret" ></span>
+                </button>
+                <ul class="dropdown-menu">
+                    <li class="dropdown-checkbox">
+                        <input type="checkbox" id="include_spring_tests" checked
+                               @change="updateFunc" v-model="includeSpring"/>
+                        <label for="include_spring_tests"
+                               >Include Spring tests</label>
+                    </li>
+                    <li class="dropdown-checkbox">
+                        <input type="checkbox" id="include_non_spring_tests" checked
+                               @change="updateFunc" v-model="includeNonSpring"/>
+                        <label for="include_non_spring_tests"
+                               >Include non Spring tests</label>
+                    </li>
+                </ul>
+            </div>
+            <div class="button-group input-group" data-toggle="tooltip"
+                 title="Filter tests based on their success">
+                <button type="button" class="form-control dropdown-toggle" data-toggle="dropdown"
+                        style="white-space: nowrap;">
+                    Test success<span class="caret" ></span>
+                </button>
+                <ul class="dropdown-menu">
+                    <li class="dropdown-checkbox">
+                        <input type="checkbox" id="include_succeeded_tests" checked
+                               @change="updateFunc" v-model="includeSuccess"/>
+                        <label for="include_succeeded_tests"
+                               >Include succeeded tests</label>
+                    </li>
+                    <li class="dropdown-checkbox">
+                        <input type="checkbox" id="include_partial_tests" checked
+                               @change="updateFunc" v-model="includePartial"/>
+                        <label for="include_partial_tests"
+                               >Include partial succeeded tests</label>
+                    </li>
+                    <li class="dropdown-checkbox">
+                        <input type="checkbox" id="include_failed_tests" checked
+                               @change="updateFunc" v-model="includeFailure"/>
+                        <label for="include_failed_tests"
+                               >Include failed tests</label>
+                    </li>
+                </ul>
+            </div>
+            <button type="button" class="btn btn-light input-group" v-on:click="clearClicked()" style="background-color: #FFFFFF;">Clear</button>
         </div>
     </div>
 </template>
