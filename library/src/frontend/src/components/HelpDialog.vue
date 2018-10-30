6d3ffc242b516ba4678893302b60390c587c02be
Florian Lüdiger <florian.luediger@googlemail.com>
2018-10-30 18:18:47 +0100

    Create modal to contain the helpful information

---
+++ library/src/frontend/src/components/HelpDialog.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="modal-mask">
+        <div class="modal-wrapper">
+            <div class="modal-container">
+
+                <div class="modal-header">
+                    <slot name="header">
+                        <h1>Help</h1>
+                    </slot>
+                </div>
+
+                <div class="modal-body">
+                    <slot name="body">
+                        Example text
+                    </slot>
+                </div>
+
+                <div class="modal-footer">
+                    <slot name="footer">
+                        <button class="modal-default-button btn btn-light" @click="$emit('close')">
+                            OK
+                        </button>
+                    </slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .modal-mask {
+        position: fixed;
+        z-index: 9998;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, .5);
+        display: table;
+    }
+
+    .modal-wrapper {
+        display: table-cell;
+        vertical-align: middle;
+    }
+
+    .modal-container {
+        width: 600px;
+        margin: 0px auto;
+        padding: 20px 30px;
+        background-color: #fff;
+        border-radius: 2px;
+        box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+    }
+</style>
