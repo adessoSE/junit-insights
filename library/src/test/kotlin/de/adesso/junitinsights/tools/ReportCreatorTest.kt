8374ec810af24592a29fde74832797615c7b5dd7
Florian Lüdiger <florian.luediger@googlemail.com>
2018-11-06 16:59:17 +0100

    Create basic structure for implementing unit tests

---
+++ library/src/test/kotlin/de/adesso/junitinsights/tools/ReportCreatorTest.kt
@@ -0,0 +1,12 @@
+package de.adesso.junitinsights.tools
+
+import org.junit.jupiter.api.Test
+
+class ReportCreatorTest {
+
+    @Test
+    fun exampleTest() {
+        print("Test is being executed")
+        assert(true)
+    }
+}
